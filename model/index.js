e875a847b224e2956292cb0f3cc3e4b62773c825
Dimitris <dimitrios.koukas@gmail.com>
2022-03-21 22:48:59 +0200

    other changes

--- model/index.js
+++ model/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Directory = require('../model/directory');
 
-mongoose.connect('mongodb://localhost:27017/projectsIB');
+mongoose.connect('mongodb+srv://admin-dimitris:testDB123@cluster0.xw0cl.mongodb.net/projectsIB');
 
 const directory = new Directory ({
     name: "40 East 88th Street, Apt3A",
